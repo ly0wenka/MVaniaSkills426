e8542b54ed02fa7c91598be6d5aa724bab8e7156
Ly0w <ly0wenka@gmail.com>
2024-01-26 08:42:30 +0200

    118

--- Source/MVaniaSkills426/MVTypes.h
+++ Source/MVaniaSkills426/MVTypes.h
@@ -18,30 +18,30 @@
 UENUM(BlueprintType, meta = (Bitflags, UseEnumValuesAsMaskValuesInEditor = "true"))
 enum class E_BMagic : uint8
 {
-    None = 0,
-    Fireball = 1 << 3,
-    ElectroSpark = 1 << 4,
-    Bloodlust = 1 << 5,
-    ArcticBlast = 1 << 6,
+    None = 0x0,
+    Fireball = 0x3,
+    ElectroSpark = 0x4,
+    Bloodlust = 0x5,
+    ArcticBlast = 0x6,
 };
 ENUM_CLASS_FLAGS(E_BMagic);
 
 UENUM(BlueprintType, meta = (Bitflags, UseEnumValuesAsMaskValuesInEditor = "true"))
 enum class E_WMagic : uint8
 {
-    None = 0,
-    LensOfTruth = 1 << 7,
-    Mist = 2 << 0,
-    Shield = 2 << 1,
-    TimeSlow = 2 << 2
+    None = 0x0,
+    LensOfTruth = 0x7,
+    Mist = 0x8,
+    Shield = 0x9,
+    TimeSlow = 0xa
 };
 ENUM_CLASS_FLAGS(E_WMagic);
 
 UENUM(BlueprintType, meta = (Bitflags, UseEnumValuesAsMaskValuesInEditor = "true"))
 enum class E_Magic_Type : uint8
 {
-    WMagic = 0,
-    BMagic = 1 << 0,
+    WMagic = 0x0,
+    BMagic = 0x1,
 };
 ENUM_CLASS_FLAGS(E_Magic_Type);
 
@@ -49,16 +49,16 @@ UENUM(BlueprintType, meta = (Bitflags, UseEnumValuesAsMaskValuesInEditor = "true
 enum class E_Skills : uint8
 {
     None = 0,
-    DoubleJump = 1 << 1,
-    BackDash = 1 << 2,
-    Fireball = E_BMagic::Fireball,
-    ElectroSpark = 1 << 4,
-    Bloodlust = 1 << 5,
-    ArcticBlast = 1 << 6,
-    LensOfTruth = E_WMagic::LensOfTruth,
-    Mist = 2 << 0,
-    Shield = 2 << 1,
-    TimeSlow = 2 << 2,
+    DoubleJump = 0x1,
+    BackDash = 0x2,
+    Fireball = 0x3,
+    ElectroSpark = 0x4,
+    Bloodlust = 0x5,
+    ArcticBlast = 0x6,
+    LensOfTruth = 0x7,
+    Mist = 0x8,
+    Shield = 0x9,
+    TimeSlow = 0xa,
     WMagic = LensOfTruth | Mist | Shield | TimeSlow,
     BMagic = Fireball | ElectroSpark | Bloodlust | ArcticBlast,
 };
